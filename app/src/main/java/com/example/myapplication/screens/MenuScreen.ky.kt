4dc4530b6ed604cde154ceb479eee739660220bf
miri <mi040925@naver.com>
2026-03-29 21:39:58 +0900

    UI 오류 수정

--- app/src/main/java/com/example/myapplication/screens/MenuScreen.ky.kt
+++ app/src/main/java/com/example/myapplication/screens/MenuScreen.ky.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -58,9 +60,9 @@ fun MenuScreen(
 
 @Composable
 private fun MenuLine(text: String, onClick: () -> Unit) {
-    androidx.compose.foundation.layout.Row(
+    Row(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .clickable(onClick = onClick),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
